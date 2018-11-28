888fa9638d108ce8d669dd5467a3a750631b3bc3
Evan Crane <evanf.crane@gmail.com>
2018-11-27 19:46:31 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-function mainFunc() {
-    console.log("input starting...");
-    const inputs = require('./src/input.js');
-    let inputFilePath = inputs.getFilePath();
-    console.log("parsefile starting...");
-    const parseFile = require('./src/parseFile.js');
-    let fileObjects = parseFile.getFileObjects(inputFilePath);
-    if (fileObjects === null) {
-        console.log("Please try again");
-        mainFunc();
-    }
-    let inputWord = inputs.getWord();
-    const parseWord = require('./src/parseWord.js');
-    let wordContent = parseWord.getParsedWord(inputWord, fileObjects[0]);
-    console.log("Ended parsing process");
-}
+function mainFunc () {
+	console.log('input starting...');
+	const inputs = require('./src/input.js');
+	let inputFilePath = inputs.getFilePath();
 
-console.log("START: APP STARTING");
+	console.log('parsing starting...');
+	const parse = require('./src/parsing/parse.js');
+	let fileObjects = parse.parseFileInputs(inputFilePath);
+	if (fileObjects === null) {
+		console.log('Please try again');
+		mainFunc();
+	}
+	let inputWord = inputs.getWord();
+	const parseWord = require('./src/parsing/parseWord.js');
+	//let wordContent = parseWord.getParsedWord(inputWord, fileObjects[0]);
+	console.log('Ended parsing process');
+}
+console.log('START: APP STARTING');
 mainFunc();
