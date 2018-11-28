aa2afef91cbe75a012af7b02a3630084eefdcc57
[Evan Crane] <[evanf.crane@gmail.com]>
2018-11-28 03:24:31 -0500

    changed custom error handling. Added module export for unit testing

--- src/parsing/parse.js
+++ src/parsing/parse.js
@@ -1,6 +1,7 @@
 //parse.js
 //file controlling the parsing of program inputs
 const fs = require('fs');
+const error = require('../models/error.js');
 const parseFile = require('./parseFile.js');
 
 function parseFileInputs (inputFilePath) {
@@ -20,10 +21,12 @@ function parseWord (inputWord) {
 function parseFilePath (inputFilePath) {
 	inputFilePath = inputFilePath.trim();
 	if (fs.existsSync(inputFilePath) && inputFilePath.slice(-3).toUpperCase() === '.TM') {
-		let fileLines = fs.readFileSync(inputFilePath);
+		let fileLines = fs.readFileSync(inputFilePath).toString().match(/^.+$/gm);
 		return fileLines;
-	} else { throw new Error({ name: 'parseFileError', message: 'Bad filepath detected' }); }
+	} else { throw new error.CustomError('parseFileError', 'Bad filepath detected or incorrect file extension'); }
 }
 
 module.exports.parseFileInputs = parseFileInputs;
 module.exports.parseWord = parseWord;
+//testing module exports
+module.exports.parseFilePath = parseFilePath;
