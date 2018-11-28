e3484e69f95f1542b28eaf919376454d47337593
[Evan Crane] <[evanf.crane@gmail.com]>
2018-11-28 03:23:40 -0500

    changed file styling

--- src/models/config.js
+++ src/models/config.js
@@ -2,14 +2,14 @@
 //defines classes and models for turing assembler configuration
 
 class Configs {
-    constructor(states, start, accept, reject, alpha, tapeAlpha) {
-        this.states = states;
-        this.start = start;
-        this.accept = accept;
-        this.reject = reject;
-        this.alpha = alpha;
-        this.tapeAlpha = tapeAlpha;
-    }
+	constructor (states, start, accept, reject, alpha, tapeAlpha) {
+		this.states = states;
+		this.start = start;
+		this.accept = accept;
+		this.reject = reject;
+		this.alpha = alpha;
+		this.tapeAlpha = tapeAlpha;
+	}
 }
 
-module.exports = Configs;
+module.exports.Configs = Configs;
