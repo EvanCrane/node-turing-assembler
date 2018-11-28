35de1bd73e2e947986d74b8a4937d1909f56fef2
Evan Crane <evanf.crane@gmail.com>
2018-11-27 19:48:44 -0500

    Changed input to follow JS style guides

--- src/input.js
+++ src/input.js
@@ -1,11 +1,11 @@
 const readlineSync = require('readline-sync');
 
-function getFilePath() {
-    return readlineSync.question('Please enter a path to the source file: ');
+function getFilePath () {
+	return readlineSync.question('Please enter a path to the source file: ');
 }
 
-function getWord() {
-    return readlineSync.question('Please enter a word: ');
+function getWord () {
+	return readlineSync.question('Please enter a word: ');
 }
 
 module.exports.getFilePath = getFilePath;
