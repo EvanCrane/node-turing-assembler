03e258538f8ddab724522a4395467e3a4ac5c505
[Evan Crane] <[evanf.crane@gmail.com]>
2018-11-29 21:05:05 -0500

    Tested parseFile -tested command parsing for default test -tested if there are no commands in file -changed using obj literal to using model defined objects -gave command object a direction parameter which defines if moving right

--- src/parsing/parseFile.js
+++ src/parsing/parseFile.js
@@ -1,28 +1,26 @@
 //parseFile.js
 
 const Configs = require('../models/config.js');
-const command = require('../models/commands.js');
+const Command = require('../models/commands.js');
 const error = require('../models/error.js');
 const configsList = ['states', 'start', 'accept', 'reject', 'alpha', 'tape-alpha'];
-const commands = ['rwRt', 'rwLt', 'rRl', 'rLl', 'rRt', 'rLt'];
+const commands = ['rwRt', 'rwLt', 'rRt', 'rLt', 'rRl', 'rLl'];
 
 function getFileObjects (fileLines) {
 	//check for bad file content
 	if (fileLines === undefined || fileLines === null || fileLines.length < 1) {
 		throwCustomParseError('parseFileObjectsError', 'ERROR: INVALID FILE CONTENT');
 	}
-	let parsedContent = parseFileLines(fileLines);
-	let configs = parseConfigs(parsedContent[0]);
-	//check for bad or missing configs
+	var [configs, commands]  = parseFileLines(fileLines);
 	if (configs === undefined || configs === null) {
 		throwCustomParseError('parseFileObjectsError', 'ERROR:MISSING OR INVALID INITIALIZATION CONFIGS');
 	}
-	let commands = parseCommands(parsedContent[1], configs);
-	//check for bad or missing commands
-	if (commands === undefined || commands === null) {
-		throwCustomParseError('parseFileObjectsError', 'ERROR: INVALID COMMAND OBJECTS');
+	//let commands = parseCommands(cmdObj, configs);
+	//check for bad command parsing
+	if (commands === undefined) {
+		throwCustomParseError('parseFileObjectsError', 'ERROR: COMMAND PARSING ERROR');
 	}
-	return (configs, commands);
+	return [configs, commands];
 }
 
 function parseFileLines (fileLines) {
@@ -40,14 +38,14 @@ function parseFileLines (fileLines) {
 	let cmdMatches = fileContent.match(/\.*?(rwRt|rwLt|rRl|rLl|rRt|rLt)[^;]*/gm);
 	let configsObj = parseConfigs(parenMatches);
 	if (cmdMatches === null) {
-		return (configsObj, null);
+		return [configsObj, null];
 	}
 	let cmdObj = parseCommands(cmdMatches, configsObj);
-	return (configsObj, cmdObj);
+	return [configsObj, cmdObj];
 }
 
 function parseConfigs (parenMatches) {
-	var configs = {};
+	var configs = new Configs();
 	for (var i = 0; i < parenMatches.length; i++) {
 		//remove matching parens
 		parenMatches[i] = parenMatches[i].replace(/\{|\}/gm, '');
@@ -147,77 +145,83 @@ function parseCommands (cmdMatches, configsObj) {
 		let mSections = newMatch.split(',');
 		if (commands.indexOf(mSections[0]) > -1) {
 			switch (mSections[0]) {
+			//rwRt
 			case commands[0]:
 				if (mSections[5] === undefined || mSections[5] === '') {
 					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], mSections[3], mSections[4])) {
-						let cmd = new command.Command(commands[0], [mSections[1], mSections[2], mSections[3], mSections[4]);
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2], mSections[3], mSections[4]];
-						cmdObjList.push(command);
+						//(rwRt, q, t, t', q')
+						//in state q where the head is reading t off of the tape, write t', transition to state q' and move the head right.
+						let cmd = new Command(commands[0], mSections[1], mSections[2], mSections[3], mSections[4], true);
+						cmdList.push(cmd);
 					}
 				} else {
 					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
 				}
 				break;
+			//rwLt
 			case commands[1]:
 				if (mSections[5] === undefined || mSections[5] === '') {
 					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], mSections[3], mSections[4])) {
-						let command = new Command();
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2], mSections[3], mSections[4]];
-						cmdObjList.push(command);
+						//(rwLt, q, t, t', q')
+						//in state q where the head is reading t off of the tape, write t', transition to state q' and move the head left.
+						let cmd = new Command(commands[1], mSections[1], mSections[2], mSections[3], mSections[4], false);
+						cmdList.push(cmd);
 					}
 				} else {
 					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
 				}
 				break;
+			//rRt
 			case commands[2]:
-				if (mSections[3] === undefined || mSections[3] === '') {
-					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], null, null)) {
-						let command = new Command();
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2]];
-						cmdObjList.push(command);
+				if (mSections[4] === undefined || mSections[4] === '') {
+					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], null, mSections[3])) {
+						//(rRt, q, t, t, q')
+						//in state q where the head is reading t off of the tape, write t, transition to state q'and move the head right.
+						let cmd = new Command(commands[2], mSections[1], mSections[2], mSections[2], mSections[3], true);
+						cmdList.push(cmd);
 					}
 				} else {
 					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
 				}
 				break;
+			//rLt
 			case commands[3]:
+				if (mSections[4] === undefined || mSections[4] === '') {
+					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], null, mSections[3])) {
+						//(rLt, q, t, t, q')
+						//in state q where the head is reading t off of the tape, write t, transition to state q'and move the head left.
+						let cmd = new Command(commands[3], mSections[1], mSections[2], mSections[2], mSections[3], false);
+						cmdList.push(cmd);
+					}
+				} else {
+					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
+				}
+				break;
+			//rRl
+			case commands[4]:
 				if (mSections[3] === undefined || mSections[3] === '') {
 					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], null, null)) {
-						let command = new Command();
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2]];
-						cmdObjList.push(command);
+						//(rRl, q, t, t, q)
+						//in state q where the head is reading t off of the tape, write t, transition to state q and move the head right.
+						let cmd = new Command(commands[4], mSections[1], mSections[2], mSections[2], mSections[1], true);
+						cmdList.push(cmd);
+					}
+				} else {
+					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
+				}
+				break;
+			//rLl
+			case commands[5]:
+				if (mSections[3] === undefined || mSections[3] === '') {
+					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], null, null)) {
+						//(rLl, q, t, t, q)
+						//in state q where the head is reading t off of the tape, write t, transition to state q and move the head left.
+						let cmd = new Command(commands[5], mSections[1], mSections[2], mSections[2], mSections[1], false);
+						cmdList.push(cmd);
 					}
 				} else {
 					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
 					return null;
-				}
-				break;
-			case commands[4]:
-				if (mSections[4] === undefined || mSections[4] === '') {
-					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], mSections[3], null)) {
-						let command = new Command();
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2], mSections[3]];
-						cmdObjList.push(command);
-					}
-				} else {
-					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
-				}
-				break;
-			case commands[5]:
-				if (mSections[4] === undefined || mSections[4] === '') {
-					if (verifyCommand(configsObj, mSections[0], mSections[1], mSections[2], mSections[3], null)) {
-						let command = new Command();
-						command.cmd = commands[0];
-						command.params = [mSections[1], mSections[2], mSections[3]];
-						cmdObjList.push(command);
-					}
-				} else {
-					throwCustomParseError('parseCommandError', 'ERROR: INVALID ARGUMENTS FOR CMD: ' + cmdMatches[i]);
 				}
 				break;
 			default:
@@ -226,20 +230,20 @@ function parseCommands (cmdMatches, configsObj) {
 			}
 		}
 	}
-	return cmdObjList;
+	return cmdList;
 }
 
 function verifyCommand (configsObj, cmd, stateParam1, tapeParam1, tapeParam2, stateParam2) {
 	if (configsObj.states.indexOf(stateParam1) > -1) {
-		if (tapeParam1 === '_' || configsObj.tapeAlpha.indexOf(tapeParam2) > -1) {
+		if (tapeParam1 === '_' || configsObj.tapeAlpha.indexOf(tapeParam1) > -1) {
 			if (cmd === 'rRl' || cmd === 'rLl') {
 				return true;
 			}
-			if (tapeParam2 === '_' || configsObj.tapeAlpha.indexOf(tapeParam2) > -1) {
+			if (configsObj.states.indexOf(stateParam2) > -1) {
 				if (cmd === 'rRt' || cmd === 'rLt') {
 					return true;
 				}
-				if (configsObj.states.indexOf(stateParam2) > -1) {
+				if (tapeParam2 === '_' || configsObj.tapeAlpha.indexOf(tapeParam2) > -1) {
 					if (cmd === 'rwRt' || cmd === 'rwLt') {
 						return true;
 					}
@@ -247,7 +251,8 @@ function verifyCommand (configsObj, cmd, stateParam1, tapeParam1, tapeParam2, st
 			}
 		}
 	}
-	throwCustomParseError('verifyCommandError', 'ERROR: BAD COMMAND ARGUMENTS');
+	let fullCmd = cmd.toString() + ' ' + String(stateParam1) + ' ' + String(tapeParam1) + ' ' + String(tapeParam2) + ' ' + String(stateParam2);
+	throwCustomParseError('verifyCommandError', 'ERROR: BAD COMMAND ARGUMENTS: CMD: ' + fullCmd);
 }
 
 function throwCustomParseError (nameStr, messageStr) {
